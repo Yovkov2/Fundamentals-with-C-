30d80b8a0a2270287a52fe75f2497d689ad28a22
YovkovIT <yovkovyoan88@gmail.com>
2024-03-14 11:54:06 +0200

    a

---
+++ BasicSyntax/Login/Program.cs
@@ -0,0 +1,23 @@
+﻿string userName = Console.ReadLine();
+string password = new string(userName.Reverse().ToArray());
+int counts = 0; 
+bool logged = false;
+while(counts != 4)
+{
+    string name = Console.ReadLine();
+    counts++;
+    if(name != password)
+    {
+        Console.WriteLine($"Incorrect password. Try again.");
+    }
+    else
+    {
+        Console.WriteLine($"User {userName} logged in.");
+        logged = true;
+        break;
+    }
+}
+if (!logged)
+{
+    Console.WriteLine($"User {userName} blocked!");
+}
---
+++ BasicSyntax/Vacation/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+
+int countOfPeople = int.Parse(Console.ReadLine());
+string typeOfGroup = Console.ReadLine();
+string dayOfWeek = Console.ReadLine(); 
+double sum = 0;
+if(typeOfGroup == "Students")
+{
+    if(dayOfWeek == "Friday")
+    {
+        sum += countOfPeople * 8.45;
+    }
+    else if(dayOfWeek == "Saturday")
+    {
+        sum += countOfPeople * 9.80;
+    }
+    else
+    {
+        sum += countOfPeople * 10.46;
+    }
+    if(countOfPeople >= 30)
+    {
+        sum -= sum * 0.15;
+    }
+}
+else if (typeOfGroup == "Business")
+{
+    if(countOfPeople >= 100)
+    {
+        countOfPeople -= 10;
+    }
+    if (dayOfWeek == "Friday")
+    {
+        sum += countOfPeople * 10.90;
+    }
+    else if (dayOfWeek == "Saturday")
+    {
+        sum += countOfPeople * 15.60;
+    }
+    else
+    {
+        sum += countOfPeople * 16;
+    }
+}
+else
+{
+    if (dayOfWeek == "Friday")
+    {
+        sum += countOfPeople * 15;
+    }
+    else if (dayOfWeek == "Saturday")
+    {
+        sum += countOfPeople * 20;
+    }
+    else
+    {
+        sum += countOfPeople * 22.50;
+    }
+    if(countOfPeople >= 10 && countOfPeople <= 20)
+    {
+        sum *= 0.05;
+    }
+}
+Console.WriteLine($"Total price: {sum:f2}");
