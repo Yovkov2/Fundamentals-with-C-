7995b1759abe230a043aa9e78bb1200e22467b4a
YovkovIT <yovkovyoan88@gmail.com>
2024-03-23 19:44:44 +0200

    more Tasks

---
+++ Lists/Append Arrays/Program.cs
@@ -0,0 +1,11 @@
+﻿List<string> integers = Console.ReadLine().Split('|').ToList();
+List<string> result = new List<string>();
+for (int i = integers.Count-1; i >= 0; i--)
+{
+    var currentChar = integers[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+	foreach (var item in currentChar)
+	{
+		result.Add(item);
+	}
+}
+Console.WriteLine(string.Join(" ",result));
---
+++ Lists/List Operations/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading.Tasks;
+
+List<int> integers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+string command = Console.ReadLine();
+while(command != "End")
+{
+    string[] type = command.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+    if (type[0] == "Add")
+    {
+        int number = int.Parse(type[1]);
+        integers.Add(number);
+    }
+    else if (type[0] == "Insert")
+    {
+        int value = int.Parse(type[1]);
+        int index = int.Parse(type[2]);
+        integers.Insert(index, value);
+    }
+    else if (type[0] == "Shift")
+    {
+       string direction = type[1];
+        if(direction == "left")
+        {
+            int task = int.Parse(type[2]);
+            while (task != 0)
+            {
+                int currentnumber = integers[0];
+                integers.RemoveAt(0);
+                integers.Add(currentnumber);
+                task--;
+            }
+        }
+        else if(direction == "right")
+        {
+            int task = int.Parse(type[2]);
+            while (task != 0)
+            {
+                int currentnumber = integers[integers.Count - 1];
+                integers.RemoveAt(integers.Count - 1);
+                integers.Insert(0, currentnumber);
+                task--;
+            }
+        }
+    }
+    else if (type[0] == "Remove")
+    {
+        int task = int.Parse(type[1]);
+        if(task >= 0 && task < integers.Count)
+        {
+            integers.RemoveAt(task);
+        }
+        else
+        {
+            Console.WriteLine("Invalid index");
+        }
+    }
+    command = Console.ReadLine();
+}
+Console.WriteLine($"{string.Join(" ", integers)}");
