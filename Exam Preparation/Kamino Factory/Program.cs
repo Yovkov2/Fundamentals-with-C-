063d3754d576edafd703a3784cf4e3c2352b526a
YovkovIT <yovkovyoan88@gmail.com>
2024-04-01 19:24:19 +0300

    Array tasks

---
+++ Exam Preparation/Kamino Factory/Program.cs
@@ -0,0 +1,54 @@
+﻿int count = int.Parse(Console.ReadLine());
+
+string command = Console.ReadLine();
+
+int applicationMaxSeqauence = 0;
+int mostLeftIndex = int.MaxValue;
+int maxSumOfOnes = 0;
+
+int bestDna = 1;
+int currentDna = 0;
+
+int[] result = null;
+
+while (command != "Clone them!")
+{
+    int[] numbers = command
+                    .Split("!", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+    int sumOfOnes = 0;
+    int maxSeauence = 0;
+    int currentSequence = 0;
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] == 0)
+        {
+            currentSequence = 0;
+            continue;
+        }
+        sumOfOnes++;
+        currentSequence++;
+        if (currentSequence > maxSeauence)
+        {
+            maxSeauence = currentSequence;
+        }
+    }
+    string targetString = new string('1', maxSeauence);
+    int currentIndex = string.Join("", numbers).IndexOf(targetString);
+    currentDna++;
+    if (maxSeauence >= applicationMaxSeqauence && currentIndex < mostLeftIndex ||
+                    maxSeauence == applicationMaxSeqauence && currentIndex == mostLeftIndex && sumOfOnes > maxSumOfOnes)
+    {
+        applicationMaxSeqauence = maxSeauence;
+        mostLeftIndex = currentIndex;
+        maxSumOfOnes = sumOfOnes;
+        bestDna = currentDna;
+        result = numbers;
+    }
+    command = Console.ReadLine();
+}
+Console.WriteLine($"Best DNA sample {bestDna} with sum: {maxSumOfOnes}.");
+Console.WriteLine(string.Join(" ", result));
---
+++ Exam Preparation/LadyBugs/Program.cs
@@ -0,0 +1,57 @@
+﻿int sizeofArray = int.Parse(Console.ReadLine());
+
+int[] bugs = new int[sizeofArray];
+int[] bugsIndex = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+for (int i = 0; i < bugsIndex.Length; i++)
+{
+    if (bugsIndex[i] < 0 || bugsIndex[i] >= bugs.Length)
+    {
+        continue;
+    }
+    bugs[bugsIndex[i]] = 1;
+}
+string command = Console.ReadLine();
+
+while(command != "end")
+{
+    string[] commandArgs = command.Split(" ");
+    int bugIndex = int.Parse(commandArgs[0]);
+    string direction = commandArgs[1];
+    int steps = int.Parse(commandArgs[2]);
+
+    if (bugIndex < 0 || bugIndex >= bugs.Length)
+    {
+        command = Console.ReadLine();
+        continue;
+    }
+    if (bugs[bugIndex] == 0)
+    {
+        command= Console.ReadLine();
+        continue;
+    }
+    bugs[bugIndex] = 0;
+    while(true)
+    {
+        if(direction == "right")
+        {
+            bugIndex += steps;
+        }
+        else
+        {
+            bugIndex -= steps;
+        }
+        if(bugIndex < 0 || bugIndex >= bugsIndex.Length)
+        {
+            break;
+        }
+        if (bugs[bugIndex] == 1)
+        {
+            continue;
+        }
+        bugs[bugIndex] = 1;
+        break;
+    }
+    command = Console.ReadLine();
+}
+Console.WriteLine(string.Join(" ", bugs));
