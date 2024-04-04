923734b2505173d72d49726c402d7955d56f4a97
YovkovIT <yovkovyoan88@gmail.com>
2024-04-04 20:54:56 +0300

    last task
    exam is coming

---
+++ Exam Preparation/Anonymous Threat/Program.cs
@@ -0,0 +1,61 @@
+﻿List<string> listOfStrings = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+string command = string.Empty;
+while ((command = Console.ReadLine()) != "3:1")
+{
+    string[] commandArray = command.Split();
+
+    if (commandArray[0] == "merge")
+    {
+        int startIndex = int.Parse(commandArray[1]);
+        startIndex = Math.Max(startIndex, 0);
+        startIndex = Math.Min(startIndex, listOfStrings.Count - 1);
+        int endIndex = int.Parse(commandArray[2]);
+        endIndex = Math.Max(0, endIndex);
+        endIndex = Math.Min(listOfStrings.Count - 1, endIndex);
+
+        string concat = string.Empty;
+        for (int i = startIndex; i <= endIndex; i++)
+        {
+            concat = concat + listOfStrings[i];
+        }
+
+        listOfStrings.RemoveRange(startIndex, endIndex - startIndex + 1);
+        listOfStrings.Insert(startIndex, concat);
+
+    }
+
+    else if (commandArray[0] == "divide")
+    {
+        int indexToDivide = int.Parse(commandArray[1]);
+        indexToDivide = Math.Max(0, indexToDivide);
+        indexToDivide = Math.Min(indexToDivide, listOfStrings.Count - 1);
+        int partitions = int.Parse(commandArray[2]);
+
+        string stringToDivide = listOfStrings[indexToDivide];
+        int LengthOfSmallPieces = stringToDivide.Length / partitions;
+        List<string> listWithSmallPieces = new List<string>();
+
+        int startIndex = 0;
+        for (int i = 0; i < partitions; i++)
+        {
+            if (i == partitions - 1)
+            {
+                string pieceToAdd = stringToDivide.Substring(startIndex, stringToDivide.Length - startIndex);
+                listWithSmallPieces.Add(pieceToAdd);
+            }
+
+            else
+            {
+                string pieceToAdd = stringToDivide.Substring(startIndex, LengthOfSmallPieces);
+                listWithSmallPieces.Add(pieceToAdd);
+                startIndex = startIndex + LengthOfSmallPieces;
+            }
+        }
+
+        listOfStrings.RemoveAt(indexToDivide);
+        listOfStrings.InsertRange(indexToDivide, listWithSmallPieces);
+    }
+}
+
+Console.WriteLine(string.Join(" ", listOfStrings));
--- Exam Preparation/Array Manipulator/obj/Debug/net6.0/Array Manipulator.AssemblyInfo.cs
+++ Exam Preparation/Array Manipulator/obj/Debug/net6.0/Array Manipulator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Array Manipulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063d3754d576edafd703a3784cf4e3c2352b526a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4722b32b912cb05c23561167f4ce03f906bf5a04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Array Manipulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Array Manipulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
