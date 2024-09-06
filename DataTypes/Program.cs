467423370e8719625bc94115e5ebab7d81277a64
YovkovIT <143735176+Yovkov2@users.noreply.github.com>
2024-09-06 20:16:21 +0300

    Add project files.

---
+++ DataTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿string input = Console.ReadLine();
+double sum = 0;
+
+sum = Types(input, sum);
+
+static double Types(string input, double sum)
+{
+    if (input == "int")
+    {
+        int number = int.Parse(Console.ReadLine());
+        sum = number * 2;
+        Console.WriteLine(sum);
+    }
+    else if (input == "double")
+    {
+        double number = double.Parse(Console.ReadLine());
+        sum = number * 1.5;
+        Console.WriteLine(sum);
+    }
+    else
+    {
+        string text = Console.ReadLine();
+        Console.WriteLine($"${text}$");
+    }
+    return sum;
+}
