f87e1fb7c7d2562a5e3fed58fc25bb25fa9935b2
YovkovIT <143735176+Yovkov2@users.noreply.github.com>
2024-10-03 16:32:51 +0300

    demo project MVC

---
+++ Demo-Web/SoftUniDemo/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoftUniDemo.Models;
+using System.Diagnostics;
+
+namespace SoftUniDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var course = new Course{
+                Name = "C#",
+               Credits = 12
+            };
+            return View(course);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Demo-Web/SoftUniDemo/Models/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace SoftUniDemo.Models
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public int Credits { get; set; }
+    }
+}
